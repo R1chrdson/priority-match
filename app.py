4f0fcf7bd461bf69a53c3690194700d090fb20dc
r1chrdson <prostoandrey01@gmail.com>
2021-03-18 16:55:13 +0200

    Added tailwind installation via flask-assets

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from login_manager import login_manager
+from flask_assets import Environment, Bundle
+import os.path
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=os.path.abspath('static'))
 app.config['SECRET_KEY'] = 'SecretKey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = 'login'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+css = Bundle('src/css/main.css', filters='postcss', output='static/css/main.css')
+assets = Environment(app)
+assets.register('css', css)
 
 login_manager.init_app(app)
 login_manager.login_view = '/auth/login'
