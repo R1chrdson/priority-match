5629cec6e86385ebb098e877cbe298902de202a3
r1chrdson <prostoandrey01@gmail.com>
2021-03-30 09:57:52 +0300

    Updated profile template

--- models/user.py
+++ models/user.py
@@ -1,14 +1,17 @@
+import os.path
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import db
 from app import login_manager
-from flask import redirect
+from flask import redirect, url_for
 
 
 class User(UserMixin, db.Model):
     _tablename = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
+    username = db.Column(db.String(16), unique=True)
+    name = db.Column(db.String(24), default='')
+    surname = db.Column(db.String(24), default='')
     password_hash = db.Column(db.String(80))
 
     def __repr__(self):
@@ -25,6 +28,14 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    @property
+    def avatar(self):
+        avatar_src = url_for('static', filename=f'user-avatar/{self.id}.svg')
+
+        if not os.path.isfile(avatar_src):
+            avatar_src = url_for('static', filename=f'user-avatar/default-avatar.png')
+
+        return avatar_src
 
 @login_manager.user_loader
 def load_user(user_id):
--- views/main.py
+++ views/main.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 from forms import EditProfileForm
-import os.path
 
 main = Blueprint('main', __name__)
 
@@ -16,8 +15,4 @@ def index():
 def profile():
     print(current_user)
     form = EditProfileForm()
-    avatar_src = url_for('static', filename=f'user-avatar/{current_user.id}.svg')
-    if not os.path.isfile(avatar_src):
-        avatar_src = url_for('static', filename=f'user-avatar/default.svg')
-
-    return render_template('profile.html', avatar_src=avatar_src, form=form, user=current_user)
+    return render_template('profile.html', form=form, user=current_user)
